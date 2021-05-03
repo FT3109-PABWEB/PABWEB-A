d99669dfe130cb8d82cd930578a5ed1adb6e5972
Muhammad Iqbal Fauzi <A2.1900119@mhs.stmik-sumedang.ac.id>
2021-05-03 14:48:48 +0700

    penyelesaian hasil modul

---
+++ A2.1900119/Praktikum Codeigniter/Penyelesaianmodul1/mardira/application/views/hello_view.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Hello Mardira</title>
+    </head>
+    <body>
+        <h1>Hello Mardira</h1>
+        <?php
+        if (isset($mvc)) {
+            echo $mvc;
+        } ?>
+    </body>
+</html>
---
+++ A2.1900119/Praktikum Codeigniter/Penyelesaianmodul4/mardira/application/controllers/Halaman.php
@@ -0,0 +1,9 @@
+<?php
+
+class Halaman extends CI_Controller
+{
+    public function Index()
+    {
+        $this->load->view('halaman_view');
+    }
+}
