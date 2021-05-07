4d936bc12d2c1234e4a471513ec7b63ff088d5c9
muhamaddwikidarusalam <a2.1900111@mhs.stmik-sumedang.ac.id>
2021-05-07 17:44:28 +0700

    Tugas CI
    Assalamualaikum, pak saya mau ngumpulin tugas Praktikum Condeigniter

---
+++ A2.1900111/Praktikum codeigniter/dwiki/application/views/auth/login_v.php
@@ -0,0 +1,14 @@
+<h3> Halaman_login</h3>
+
+<?php echo $form_open ?>
+<p>
+        <?php echo $label_email ?>
+        <?php echo $input_email ?>
+</p>
+<p>
+    <?php echo $label_password ?>
+    <?php echo $input_password ?>
+</p>
+<?php echo $submit ?>
+<?php $form_close ?>
+<
