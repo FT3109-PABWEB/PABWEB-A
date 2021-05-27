87d1a817735e3a2520725834063fb43e1d982f1a
Demmy Ahriyan Rozak <56103001+demmyar@users.noreply.github.com>
2021-05-27 20:38:57 +0700

    "Menyelesaikan tugas CodeIgniter"
    Menyelesaikan tugas CodeIgniter 3

---
+++ a2.1900041/Praktikum CodeIgniter/CI3/mydem/application/models/Hello_model.php
@@ -0,0 +1,15 @@
+<?php
+
+class Hello_model extends CI_Model
+{
+
+    public function hello_demmy()
+    {
+        return "Hello Demmy";
+    }
+
+    public function demmy_mvc()
+    {
+        return "Ini menggunakan MVC";
+    }
+}
---
+++ a2.1900041/Praktikum CodeIgniter/CI3/mydem/application/views/row_view.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <tittle>Metode Row</tittle>
+</head>
+<h1>Metode Row</h1>
+<body>
+    <?php echo "Nama saya {$row->nama_mhs} dan NIM saya {$row->NIM}"; ?>
+</body>
+</html>
