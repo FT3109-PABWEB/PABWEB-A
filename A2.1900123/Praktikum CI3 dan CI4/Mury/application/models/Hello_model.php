06218a09c0ad2558e631d1eae80f9ece9370a57f
Murry Muryadin <muryycuayy@gmail.com>
2021-06-08 16:28:31 +0700

    Tugas Praktikum CI3 dan CI4
    Pengumpulan Tugas Praktikum CI3 dan CI4

---
+++ A2.1900123/Praktikum CI3 dan CI4/Mury/application/models/Hello_model.php
@@ -0,0 +1,14 @@
+<?php
+
+class Hello_model extends CI_Model
+{
+    public function Hello_Murry()
+    {
+        return "Hello Murry";
+    }
+
+    public function murry_mvc()
+    {
+        return "Ini menggunakan MVC";
+    }
+}
