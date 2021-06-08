24924c62dd958d7316e8ff79ea371a1657e463c8
galihpermana858 <55782384+galihpermana858@users.noreply.github.com>
2021-06-08 16:40:21 +0700

    tugas CODEIGNITER 3
    Terimakasih atas Bimbingan selama ini pak!!!

---
+++ CODEIGNITER 3/application/models/Hello_model.php
@@ -0,0 +1,14 @@
+<?php
+
+class Hello_model extends CI_Model
+{
+    public function Hello_galih()
+    {
+        return "Hello Galih";
+    }
+
+    public function Galih_mvc()
+    {
+        return "Ini menggunakan MVC";
+    }
+}
